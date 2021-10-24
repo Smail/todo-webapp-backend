b5a2b8fd0fa8a81769a43552800001de59686c8b
Smail <44433524+Smail@users.noreply.github.com>
2021-10-24 09:44:35 +0200

    Rename tables and columns and use PascalCase

--- database/sql/CreateTables.sql
+++ database/sql/CreateTables.sql
@@ -1,35 +1,33 @@
-CREATE TABLE IF NOT EXISTS user
+CREATE TABLE IF NOT EXISTS User
 (
-    user_id    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    username   TEXT    NOT NULL UNIQUE,
-    password   TEXT    NOT NULL,
-    first_name TEXT    NOT NULL,
-    last_name  TEXT    NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    UserId    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    Username   TEXT    NOT NULL UNIQUE,
+    PasswordHash   TEXT    NOT NULL,
+    FirstName TEXT    NOT NULL,
+    LastName  TEXT    NOT NULL,
+    CreatedAt DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
--- The projects a user has created
-CREATE TABLE IF NOT EXISTS project
+CREATE TABLE IF NOT EXISTS Project
 (
-    project_id   INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id      INTEGER     NOT NULL,
-    project_name TEXT UNIQUE NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user (user_id)
+    ProjectId   INTEGER PRIMARY KEY AUTOINCREMENT,
+    UserId      INTEGER     NOT NULL,
+    ProjectName TEXT UNIQUE NOT NULL,
+    FOREIGN KEY (UserId) REFERENCES User (UserId)
 );
 
--- The tasks that were created by the users
-CREATE TABLE IF NOT EXISTS task
+CREATE TABLE IF NOT EXISTS Task
 (
-    task_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name    TEXT NOT NULL,
-    content TEXT
+    TaskId INTEGER PRIMARY KEY AUTOINCREMENT,
+    TaskName    TEXT NOT NULL,
+    TaskContent TEXT
 );
 
-CREATE TABLE IF NOT EXISTS project_tasks
+CREATE TABLE IF NOT EXISTS ProjectTasks
 (
-    project_id INTEGER NOT NULL,
-    task_id    INTEGER NOT NULL,
-    FOREIGN KEY (project_id) REFERENCES project (project_id),
-    FOREIGN KEY (task_id) REFERENCES task (task_id),
-    PRIMARY KEY (project_id, task_id)
+    ProjectId INTEGER NOT NULL,
+    TaskId    INTEGER NOT NULL,
+    FOREIGN KEY (ProjectId) REFERENCES Project (ProjectId),
+    FOREIGN KEY (TaskId) REFERENCES Task (TaskId),
+    PRIMARY KEY (ProjectId, TaskId)
 );
