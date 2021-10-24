6bbceeef2c709d0c1236ad085c54259798fe00a3
Smail <44433524+Smail@users.noreply.github.com>
2021-10-24 09:25:45 +0200

    Add SQL files and a script, which creates the database

---
+++ database/create-database.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+sqlite3 db.sqlite3 ".read sql/CreateTables.sql"
---
+++ database/sql/CreateTables.sql
@@ -0,0 +1,35 @@
+CREATE TABLE IF NOT EXISTS user
+(
+    user_id    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    username   TEXT    NOT NULL UNIQUE,
+    password   TEXT    NOT NULL,
+    first_name TEXT    NOT NULL,
+    last_name  TEXT    NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+-- The projects a user has created
+CREATE TABLE IF NOT EXISTS project
+(
+    project_id   INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id      INTEGER     NOT NULL,
+    project_name TEXT UNIQUE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user (user_id)
+);
+
+-- The tasks that were created by the users
+CREATE TABLE IF NOT EXISTS task
+(
+    task_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name    TEXT NOT NULL,
+    content TEXT
+);
+
+CREATE TABLE IF NOT EXISTS project_tasks
+(
+    project_id INTEGER NOT NULL,
+    task_id    INTEGER NOT NULL,
+    FOREIGN KEY (project_id) REFERENCES project_id (project_id),
+    FOREIGN KEY (task_id) REFERENCES task_id (task_id),
+    PRIMARY KEY (project_id, task_id)
+);
