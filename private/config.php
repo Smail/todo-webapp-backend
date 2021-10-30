a87c962cbac859d6fef659e7487ac52a777e6db4
Smail <44433524+Smail@users.noreply.github.com>
2021-10-30 13:36:36 +0200

    Set Access-Control-Allow-Origin always to the client source

--- private/config.php
+++ private/config.php
@@ -4,6 +4,8 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 session_start();
 
+header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+
 function is_logged_in(): bool {
     return (isset($_SESSION['is_logged_in']) && $_SESSION['is_logged_in']);
 }
